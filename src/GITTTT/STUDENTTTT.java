995a395901150e83588c9852dbd68dac66f1f905
almasahmet27@hotmail.com <newrozkerem1@>
2023-03-18 14:11:14 +0100

    commit denemesi yapiliyor yapildi

--- src/GITTTT/STUDENTTTT.java
+++ src/GITTTT/STUDENTTTT.java
@@ -3,6 +3,6 @@ package GITTTT;
 public class STUDENTTTT {
     public static void main(String[] args) {
         System.out.println("merhaba");
-        System.out.println("");
+        System.out.println("hggg");
     }
 }
